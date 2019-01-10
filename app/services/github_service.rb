4102e025fd7bd57a74be972b830600a8adfc29bb
github username <github email address>
2019-01-10 18:56:09 -0500

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,33 +1,47 @@
 class GithubService
 
-  attr_reader :access_token
+attr_reader :access_token
 
   def initialize(access_hash = nil)
     @access_token = access_hash["access_token"] if access_hash
   end
 
   def authenticate!(client_id, client_secret, code)
-    response = Faraday.post "https://github.com/login/oauth/access_token",
-        {client_id: client_id, client_secret: client_secret, code: code},
-        {'Accept' => 'application/json'}
-    access_hash = JSON.parse(response.body)
-    @access_token = access_hash["access_token"]
+
+    response = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+      req.body = {client_id: client_id, client_secret: client_secret, code: code}
+      req.headers['Accept'] = 'application/json'
+    end
+      access_hash = JSON.parse(response.body)
+      @access_token = access_hash["access_token"]
   end
 
   def get_username
-    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    user_response = Faraday.get "https://api.github.com/user" do |req|
+      req.headers['Authorization'] = 'token ' + @access_token
+      req.headers['Accept'] = 'application/json'
+    end
     user_json = JSON.parse(user_response.body)
-    user_json["login"]
+    user_json['login']
   end
 
   def get_repos
-    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    response = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.headers['Authorization'] = 'token ' + @access_token
+      req.headers['Accept'] = 'application/json'
+    end
     repos_array = JSON.parse(response.body)
-    repos_array.map{|repo| GithubRepo.new(repo) }
+    repos_array.map {|repo|GithubRepo.new(repo)}
   end
 
+
   def create_repo(name)
-    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    response = Faraday.post "https://api.github.com/user/repos" do |req|
+      req.headers['Authorization'] = 'token ' + @access_token
+      req.headers['Accept'] = 'application/json'
+      req.body = {name: name}.to_json
+    end
   end
+
 
 end
