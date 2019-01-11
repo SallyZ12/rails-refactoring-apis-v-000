1741fa050f5824e025f22044f25671276d97546b
github username <github email address>
2019-01-10 19:49:36 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,10 @@ class SessionsController < ApplicationController
       github = GithubService.new
       #retrieve session token
       session[:token] = github.authenticate!(ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_SECRET'], params[:code])
-      
+
       #retreive user name
       session[:user_name] = github.get_username
 
       redirect_to root_path
     end
-
   end
