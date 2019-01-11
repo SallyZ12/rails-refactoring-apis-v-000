88a2869d3da5fa33b94961bf6fadaae420d02e5a
github username <github email address>
2019-01-10 19:52:27 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
+
   def create
       github = GithubService.new
       #retrieve session token
